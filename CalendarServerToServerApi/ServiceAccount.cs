36894c1df3c067c4e725464c1e8d86ee3fa79cbe
shivam srivastava <shivam.s@hbwgurgaon.com>
2017-10-04 15:53:29 +0530

    Complete Project
    commited

---
+++ CalendarServerToServerApi/ServiceAccount.cs
@@ -0,0 +1,101 @@
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Calendar.v3.Data;
+
+namespace GoogleSamplecSharpSample.Calendarv3.Auth
+{
+    //public class ServiceAccountJson
+    //{
+    //    public string type { get; set; }
+    //    public string project_id { get; set; }
+    //    public string private_key_id { get; set; }
+    //    public string private_key { get; set; }
+    //    public string client_email { get; set; }
+    //    public string client_id { get; set; }
+    //    public string auth_uri { get; set; }
+    //    public string token_uri { get; set; }
+    //    public string auth_provider_x509_cert_url { get; set; }
+    //    public string client_x509_cert_url { get; set; }
+    //}
+
+   
+
+    public static class ServiceAccountExample
+    {
+
+        /// <summary>
+        /// Authenticating to Google using a Service account
+        /// Documentation: https://developers.google.com/accounts/docs/OAuth2#serviceaccount
+        /// </summary>
+        /// <param name="serviceAccountEmail">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="serviceAccountCredentialFilePath">Location of the .p12 or Json Service account key file downloaded from Google Developer console https://console.developers.google.com</param>
+        /// <returns>AnalyticsService used to make requests against the Analytics API</returns>
+       
+        public static CalendarService AuthenticateServiceAccount(string serviceAccountEmail, string serviceAccountCredentialFilePath, string[] scopes)
+		{
+            try
+            {
+                if (string.IsNullOrEmpty(serviceAccountCredentialFilePath))
+                    throw new Exception("Path to the service account credentials file is required.");
+                if (!File.Exists(serviceAccountCredentialFilePath))
+                    throw new Exception("The service account credentials file does not exist at: " + serviceAccountCredentialFilePath);
+                if (string.IsNullOrEmpty(serviceAccountEmail))
+                    throw new Exception("ServiceAccountEmail is required.");
+               
+                // For Json file
+                if (Path.GetExtension(serviceAccountCredentialFilePath).ToLower() == ".json")
+                {
+                    GoogleCredential credential;
+                    //using(FileStream stream = File.Open(serviceAccountCredentialFilePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                    
+
+                    using (var stream = new FileStream(serviceAccountCredentialFilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        credential = GoogleCredential.FromStream(stream)
+                             .CreateScoped(scopes).CreateWithUser("k.sachdeva@abroadshiksha.com");
+                    }
+                   
+                    // Create the  Calendar service.
+                    return new CalendarService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                       // ApiKey = "AIzaSyB8vsdQeTlY79HSoZA013gJECEHrpOFkN8",
+                        ApplicationName = "Calendar Service account Authentication Sample",
+                    });
+                }
+                else if (Path.GetExtension(serviceAccountCredentialFilePath).ToLower() == ".p12")
+                {   // If its a P12 file
+
+                    var certificate = new X509Certificate2(serviceAccountCredentialFilePath, "notasecret", X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
+                    var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(serviceAccountEmail)
+                    {
+                        Scopes = scopes
+                    }.FromCertificate(certificate));
+
+                    // Create the  Calendar service.
+                    return new CalendarService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                        ApplicationName = "Calendar Authentication Sample",
+
+                    });
+                }
+                else
+                {
+                    throw new Exception("Unsupported Service accounts credentials.");
+                }
+
+            }
+            catch (Exception ex)
+            {                
+                throw new Exception("CreateServiceAccountCalendarFailed", ex);
+            }
+        }
+
+       
+    }
+}
---
+++ CalendarServerToServerApi/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Google.Apis.Calendar.v3;
+using GoogleSamplecSharpSample.Calendarv3.Auth;
+using System.IO;
+using Google.Apis.Calendar.v3.Data;
+
+namespace CalendarServerToServerApi
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //CalendarService service = new CalendarService();
+        Event myEvent = new Event
+        {
+            Summary = "Appointment by shivam",
+            Location = "Somewhere",
+            Start = new EventDateTime()
+            {
+                DateTime = new DateTime(2017, 10, 4, 2, 0, 0),
+                TimeZone = "America/Los_Angeles"
+            },
+            End = new EventDateTime()
+            {
+                DateTime = new DateTime(2017, 10, 4, 2, 30, 0),
+                TimeZone = "America/Los_Angeles"
+            }
+            //,
+            // Recurrence = new String[] {
+            //"RRULE:FREQ=WEEKLY;BYDAY=MO"
+            //}
+            //,
+            // Attendees = new List<EventAttendee>()
+            // {
+            // new EventAttendee() { Email = "johndoe@gmail.com" }
+            //}
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           string[] scopes = new string[] {
+     CalendarService.Scope.Calendar //, // Manage your calendars
+ 	//CalendarService.Scope.CalendarReadonly // View your Calendars
+ };
+            string caluser = "sudeeksha@abroadshiksha.com";
+            string filePath = Server.MapPath("~/Key/key.json");
+            //string filePath = @"D:\_Shivam\CalendarServerToServerApi\CalendarServerToServerApi\Key\key.json";
+            var service=ServiceAccountExample.AuthenticateServiceAccount("abroadshiksha@abroadhikshaservices.iam.gserviceaccount.com",filePath, scopes);
+            insert(service, caluser, myEvent);
+        }
+       
+       
+
+        public static Event insert(CalendarService service, string id, Event myEvent)
+        {
+            try
+            {
+                return service.Events.Insert(myEvent, id).Execute();
+              
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+    }
+}
